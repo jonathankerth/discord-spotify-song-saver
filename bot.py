089f33eb2e9a73f6a256528324cb7d7be2ddd000
jonathankerth <jonathanpkerth@gmail.com>
2023-11-06 15:06:19 -0800

    add firebase structure

--- bot.py
+++ bot.py
@@ -1,11 +1,31 @@
 import discord
 from discord.ext import commands
+import re
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('path/to/your/serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 TOKEN = 'YOUR_BOT_TOKEN'
-GUILD_ID = YOUR_GUILD_ID  # Replace with your server's ID
-CHANNEL_ID = YOUR_CHANNEL_ID  # Replace with the channel ID where songs are posted
+GUILD_ID = 1234
+CHANNEL_ID = 1234
 
 bot = commands.Bot(command_prefix='!')
+
+# Regex pattern to match URLs
+URL_REGEX = r"(https?://[^\s]+)"
+
+# Function to save song link to Firestore
+async def save_song_link(song_link):
+    doc_ref = db.collection('song_links').document()
+    doc_ref.set({
+        'url': song_link
+    })
 
 @bot.event
 async def on_ready():
@@ -14,9 +34,31 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    # Ensure the bot does not reply to itself
+    if message.author == bot.user:
+        return
+
+    # Check if the message is in the designated channel
     if message.channel.id == CHANNEL_ID:
-        # Simple regex to match URLs
-        if 'https://' in message.content or 'http://' in message.content:
-            print(f"Song Link Detected: {message.content}")
+        # Check if the message contains a URL using regex
+        urls = re.findall(URL_REGEX, message.content)
+        if urls:
+            for url in urls:
+                print(f"Song Link Detected: {url}")
+                await save_song_link(url)
+                await message.channel.send(f"Song link saved: {url}")
+
+    # Process commands
+    await bot.process_commands(message)
+
+# Command to retrieve saved song links
+@bot.command(name='songs', help='Displays the saved song links.')
+async def on_songs_command(ctx):
+    song_links = db.collection('song_links').stream()
+    songs = [song_link.to_dict()['url'] for song_link in song_links]
+    if songs:
+        await ctx.send("\n".join(songs))
+    else:
+        await ctx.send("No song links have been saved yet.")
 
 bot.run(TOKEN)
