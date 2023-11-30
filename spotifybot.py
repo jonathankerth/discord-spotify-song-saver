37ca24ce6de37823369cc880bd74904030d7e8bb
jonathankerth <jonathanpkerth@gmail.com>
2023-11-30 00:13:24 -0800

    Revert "remove flask"
    This reverts commit c52528c23bfb6bcc5f06a06fa99a308389034858.

--- spotifybot.py
+++ spotifybot.py
@@ -5,11 +5,16 @@ from spotipy.oauth2 import SpotifyOAuth
 import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
-
+from flask import Flask
 
 # Load environment variables
 load_dotenv()
 
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 try:
     # Initialize Firebase
