9e165611a7329955759aa8cbde83235f9222e1e0
jonathankerth <jonathanpkerth@gmail.com>
2023-11-29 23:39:27 -0800

    update spotify redirect uri

--- spotifybot.py
+++ spotifybot.py
@@ -27,7 +27,10 @@ playlist_url = (
 parts = playlist_url.split("/")
 user_id = parts[4]
 
-SPOTIFY_REDIRECT_URI = "https://your-heroku-app-name.herokuapp.com/callback"
+SPOTIFY_REDIRECT_URI = (
+    "https://discord-song-scraper-ac3a436a01d8.herokuapp.com/callback"
+)
+
 
 try:
     # Initialize Spotify Client using SpotifyOAuth
