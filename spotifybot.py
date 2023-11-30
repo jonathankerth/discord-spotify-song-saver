ccbcc9421114490a9e7d96eabc540e194ce03590
jonathankerth <jonathanpkerth@gmail.com>
2023-11-29 22:36:16 -0800

    add cache_path

--- spotifybot.py
+++ spotifybot.py
@@ -37,6 +37,7 @@ try:
             client_secret=SPOTIFY_CLIENT_SECRET,
             redirect_uri=SPOTIFY_REDIRECT_URI,
             scope=SPOTIFY_SCOPE,
+            cache_path="/tmp/.spotipyoauthcache",
         )
     )
     user_id = sp.current_user()["id"]
